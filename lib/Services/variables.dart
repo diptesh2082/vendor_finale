7e60e1890991517ff2f7c208fcb8206e9aa0473e
Diptesh Mandal <dipteshmandal555@gmail.com>
2022-05-15 20:18:09 +0530

    iiu

--- lib/Services/variables.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-// class vendors extends GetxController{
-//   RxInt bookings=0.obs;
-// }
--- lib/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class AppColors {
-  static Color bottomNaVBarTextColor = const Color(0xff292F3D);
-  static Color backgroundColor = const Color(0xffF4F4F4);
-}
